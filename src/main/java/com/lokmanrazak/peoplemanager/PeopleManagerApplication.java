1bb4e6073b1924bb1fbf743caa42c672d10b7bdc
Lokman Razak <lokman.razak@me.com>
2021-01-17 18:44:44 +0000

    Add PeopleManagerApplication

--- src/main/java/com/lokmanrazak/peoplemanager/PeopleManagerApplication.java
+++ src/main/java/com/lokmanrazak/peoplemanager/PeopleManagerApplication.java
@@ -1,6 +1,11 @@
 package com.lokmanrazak.peoplemanager;
 
+import com.lokmanrazak.peoplemanager.daos.PersonDAO;
+import com.lokmanrazak.peoplemanager.models.Person;
+import com.lokmanrazak.peoplemanager.resources.PeopleResource;
 import io.dropwizard.Application;
+import io.dropwizard.db.PooledDataSourceFactory;
+import io.dropwizard.hibernate.HibernateBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
@@ -9,13 +14,23 @@ public class PeopleManagerApplication extends Application<PeopleManagerConfigura
         new PeopleManagerApplication().run(args);
     }
 
+    private final HibernateBundle<PeopleManagerConfiguration> hibernateBundle =
+            new HibernateBundle<>(Person.class) {
+                @Override
+                public PooledDataSourceFactory getDataSourceFactory(PeopleManagerConfiguration configuration) {
+                    return configuration.getDataSourceFactory();
+                }
+            };
+
     @Override
     public void initialize(Bootstrap<PeopleManagerConfiguration> bootstrap) {
-        super.initialize(bootstrap);
+        bootstrap.addBundle(hibernateBundle);
     }
 
     @Override
     public void run(PeopleManagerConfiguration peopleManagerConfiguration, Environment environment) {
+        final PersonDAO dao = new PersonDAO(hibernateBundle.getSessionFactory());
 
+        environment.jersey().register(new PeopleResource(dao));
     }
 }

98df9e3c87f4b6c518e53e5650b1f64f490c3073
Lokman Razak <lokman.razak@me.com>
2021-01-18 04:59:35 +0000

    Add AddressResourceTest

--- src/test/java/resources/AddressResourceTest.java
+++ src/test/java/resources/AddressResourceTest.java
@@ -1,8 +1,10 @@
 package resources;
 
+import com.lokmanrazak.peoplemanager.daos.AddressDAO;
 import com.lokmanrazak.peoplemanager.daos.PersonDAO;
+import com.lokmanrazak.peoplemanager.models.Address;
 import com.lokmanrazak.peoplemanager.models.Person;
-import com.lokmanrazak.peoplemanager.resources.PeopleResource;
+import com.lokmanrazak.peoplemanager.resources.AddressResource;
 import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
 import io.dropwizard.testing.junit5.ResourceExtension;
 import org.junit.Test;
@@ -11,74 +13,68 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import java.util.Collections;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(DropwizardExtensionsSupport.class)
-public class PeopleResourceTest {
-    private static final PersonDAO DAO = mock(PersonDAO.class);
+public class AddressResourceTest {
+    private static final PersonDAO PERSON_DAO = mock(PersonDAO.class);
+    private static final AddressDAO ADDRESS_DAO = mock(AddressDAO.class);
     public static final ResourceExtension EXT = ResourceExtension.builder()
-            .addResource(new PeopleResource(DAO))
+            .addResource(new AddressResource(ADDRESS_DAO, PERSON_DAO))
             .build();
 
+    private Address address;
     private Person person;
 
     @BeforeEach
     public void setup() {
+        address = new Address();
+        address.setId(1L);
+
         person = new Person();
         person.setId(1L);
     }
 
     @AfterEach
     public void tearDown() {
-        reset(DAO);
+        reset(PERSON_DAO, ADDRESS_DAO);
     }
 
     @Test
-    public void addPerson() {
-        when(DAO.insert(any(Person.class))).thenReturn(person);
+    public void addAddress() {
+        when(PERSON_DAO.findById(anyLong())).thenReturn(person);
+        when(ADDRESS_DAO.insert(any(Address.class))).thenReturn(address);
 
-        Response response = EXT.target("/people")
+        Response response = EXT.target("/address/1")
                 .request(MediaType.APPLICATION_JSON_TYPE)
-                .post(Entity.entity(person, MediaType.APPLICATION_JSON_TYPE));
+                .post(Entity.entity(address, MediaType.APPLICATION_JSON_TYPE));
 
         assertThat(response.getStatusInfo()).isEqualTo(Response.Status.OK);
     }
 
     @Test
-    public void updatePerson() {
-        Response response = EXT.target("/people")
+    public void updateAddress() {
+        when(ADDRESS_DAO.getPerson(anyLong())).thenReturn(person);
+
+        Response response = EXT.target("address")
                 .request(MediaType.APPLICATION_JSON_TYPE)
-                .put(Entity.entity(person, MediaType.APPLICATION_JSON_TYPE));
+                .put(Entity.entity(address, MediaType.APPLICATION_JSON_TYPE));
 
         assertThat(response.getStatusInfo()).isEqualTo(Response.Status.OK);
     }
 
     @Test
-    public void deletePerson() {
-        Response response = EXT.target("/people/1")
+    public void deleteAddress() {
+        when(ADDRESS_DAO.findById(anyLong())).thenReturn(address);
+
+        Response response = EXT.target("/address/1")
                 .request()
                 .delete();
 
         assertThat(response.getStatusInfo()).isEqualTo(Response.Status.OK);
-    }
-
-    @Test
-    public void listPeople() {
-        List<Person> people = Collections.singletonList(person);
-        when(DAO.findAll()).thenReturn(people);
-
-        List<Person> response = EXT.target("/people")
-                .request().get(new GenericType<>() {
-                });
-
-        assertThat(response).containsAll(people);
     }
 }
